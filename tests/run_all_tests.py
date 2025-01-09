7e9704bcf7d5dfe3c1f43a456fc1606f97aab7f5
sg670w <sg670w@att.com>
2025-01-09 15:00:24 -0600

    test order

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,16 +1,35 @@
 import unittest
 import os
 import sys
-
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # Change the working directory to the parent directory to allow importing the package.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+def reorder_tests(tests):
+    """ Reorder tests to run import tests first, then normal tests, and finally example file tests."""
+    import_tests = []
+    example_tests = []
+    normal_tests = []
+    for test in tests:
+        if 'import' in str(test).lower():
+            import_tests.append(test)
+        elif 'example' in str(test).lower():
+            example_tests.append(test)
+        else:
+            normal_tests.append(test)
+    return import_tests + normal_tests + example_tests 
+
 if __name__ == '__main__':
     # Discover and run all tests in the 'tests' directory
     loader = unittest.TestLoader()
-    tests = loader.discover('tests')
+    suite = loader.discover('tests', pattern='test_*.py')
+    
+    # Reorder tests
+    ordered_tests = reorder_tests(list(suite))
+    ordered_suite = unittest.TestSuite(ordered_tests)
+    
     testRunner = unittest.TextTestRunner()
-    testRunner.run(tests)
+    testRunner.run(ordered_suite)
