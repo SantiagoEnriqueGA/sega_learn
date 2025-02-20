06f5986911183c8d155230f165ec8e84a5d59012
sg670w <sg670w@att.com>
2025-02-20 17:32:07 -0600

    DEV CuPy NN module

---
+++ sega_learn/neural_networks_cupy_dev/__init__.py
@@ -0,0 +1,21 @@
+from .optimizers import AdamOptimizer, SGDOptimizer, AdadeltaOptimizer
+
+from .schedulers import lr_scheduler_exp, lr_scheduler_plateau, lr_scheduler_step
+
+from .loss import CrossEntropyLoss, BCEWithLogitsLoss
+
+from .neuralNetwork import NeuralNetwork, Layer, Activation
+
+__all__ = [
+    'AdamOptimizer',
+    'SGDOptimizer',
+    'AdadeltaOptimizer',
+    'lr_scheduler_exp',
+    'lr_scheduler_plateau',
+    'lr_scheduler_step',
+    'CrossEntropyLoss',
+    'BCEWithLogitsLoss',
+    'NeuralNetwork',
+    'Layer',
+    'Activation',
+]
