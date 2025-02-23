bb35860bb5d61baa7e558daede890d3d4495b14d
SantiagoEnriqueGA <sega97@gmail.com>
2025-02-23 16:42:28 -0600

    linear_models + plots

--- tests_performance/scalability/linear_models.py
+++ tests_performance/scalability/linear_models.py
@@ -29,7 +29,11 @@ class TestTime():
     def measure_performance(self, model_class_sklearn, model_class_sega):
         def model_performance(model_class):
             start_time = time.time()
-            model = model_class()
+            if model_class == RANSAC:
+                model = model_class(n=10, k=100, t=0.05, d=self.num_samples//10,
+                                    auto_scale_t=True, scale_t_factor=2)
+            else:
+                model = model_class()
             model.fit(self.X, self.y)
             y_pred = model.predict(self.X_test)
             end_time = time.time()
@@ -70,54 +74,65 @@ def plot_results(results, title):
 
     plt.xscale("log")
     plt.title(title)
-    plt.xlabel("Sample Size")
+    plt.xlabel("Sample Size (log scale)")
     plt.ylabel("Time (s)")
     plt.legend(title="Legend", title_fontproperties={'weight': 'bold'})
     plt.tight_layout()
     plt.grid(True)
     plt.savefig(f"tests_performance/scalability/plots/linear_models_{title.replace(' ', '_')}.png")
-    plt.show()
+    # plt.show()
 
-def test_ols(num_zeros, num_runs):
-    sample_sizes = [10**i for i in range(1, num_zeros)]
-    sample_times_sega = []
-    sample_times_sklearn = []
-    sample_std_sega = []
-    sample_std_sklearn = []
 
-    for sample_size in sample_sizes:
-        # Generate synthetic data
-        X, y = synthetic_data_regression(sample_size)
-        X_test = X[:1000]  # Use a subset for testing
-        
-        timer = TestTime(X, y, X_test, sample_size, num_runs)
-        avg_sklearn_time, std_sklearn_time, avg_sega_time, std_sega_time = timer.measure_performance(linear_model.LinearRegression, OrdinaryLeastSquares)
-        
-        sample_times_sega.append(avg_sega_time)
-        sample_std_sega.append(std_sega_time)
-        sample_times_sklearn.append(avg_sklearn_time)
-        sample_std_sklearn.append(std_sklearn_time)
+def test_compare_libraries(models_to_test, num_runs):
+    for models in models_to_test:
+        sklearn_model, sega_model, num_zeros = models
+        sample_sizes = [10**i for i in range(1, num_zeros)]
+        sample_times_sega = []
+        sample_times_sklearn = []
+        sample_std_sega = []
+        sample_std_sklearn = []
 
-    # Construct DataFrame
-    results = pd.DataFrame({
-        "Sample Size": sample_sizes,
-        "sklearn": sample_times_sklearn,
-        "std_sklearn": sample_std_sklearn,
-        "sega_learn": sample_times_sega,
-        "std_sega_learn": sample_std_sega
-    })
+        print(f"\n{sega_model.__name__} and {sklearn_model.__name__} Performance comparison:")
 
-    plot_results(results, "OLS Scalability")
+        for sample_size in sample_sizes:
+            # Generate synthetic data
+            X, y = synthetic_data_regression(sample_size)
+            X_test = X[:1000]  # Use a subset for testing
+            
+            timer = TestTime(X, y, X_test, sample_size, num_runs)
+            avg_sklearn_time, std_sklearn_time, avg_sega_time, std_sega_time = timer.measure_performance(model_class_sklearn=sklearn_model, model_class_sega=sega_model)
+            
+            sample_times_sega.append(avg_sega_time)
+            sample_std_sega.append(std_sega_time)
+            sample_times_sklearn.append(avg_sklearn_time)
+            sample_std_sklearn.append(std_sklearn_time)
 
-    return results
+        # Construct DataFrame
+        results = pd.DataFrame({
+            "Sample Size": sample_sizes,
+            "sklearn": sample_times_sklearn,
+            "std_sklearn": sample_std_sklearn,
+            "sega_learn": sample_times_sega,
+            "std_sega_learn": sample_std_sega
+        })
+
+        print(results)
+        plot_results(results, f"{sega_model.__name__} Scalability")
 
 def main():   
     NUM_RUNS = 10
 
-    # Run tests
-    ols_num_zeros = 8
-    results = test_ols(ols_num_zeros, NUM_RUNS)
-    print(results)
+    models_to_test = [
+        (linear_model.LinearRegression, OrdinaryLeastSquares, 8),
+        (linear_model.Ridge, Ridge, 5),
+        (linear_model.Lasso, Lasso, 5),
+        (linear_model.BayesianRidge, Bayesian, 8),
+        (linear_model.RANSACRegressor, RANSAC, 7),
+        (linear_model.PassiveAggressiveRegressor, PassiveAggressiveRegressor, 7)
+    ]
+
+    test_compare_libraries(models_to_test, NUM_RUNS)
+
 
 if __name__ == '__main__':
     main()
