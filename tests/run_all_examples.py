907c6e6deb4d1b4c95d16bba1f4406c2f0d0ab9d
sg670w <sg670w@att.com>
2025-03-06 16:16:22 -0600

    test runner script for example tests

---
+++ tests/run_all_examples.py
@@ -0,0 +1,20 @@
+import unittest
+import os
+import sys
+
+# Change the working directory to the parent directory to allow importing the segadb package.
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def load_tests(loader, standard_tests, pattern):
+    suite = unittest.TestSuite()
+    for all_test_suite in unittest.defaultTestLoader.discover('tests', pattern='test*_examples.py'):
+        for test_suite in all_test_suite:
+            suite.addTest(test_suite)
+    return suite
+
+if __name__ == '__main__':
+    # Discover and run selected tests in the 'tests' directory
+    loader = unittest.TestLoader()
+    tests = load_tests(loader, None, 'test*.py')
+    testRunner = unittest.TextTestRunner()
+    testRunner.run(tests)
