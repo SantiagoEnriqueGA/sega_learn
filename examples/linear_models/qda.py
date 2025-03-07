957ffd98b00305bb0318664927588844873c12e7
sg670w <sg670w@att.com>
2025-03-07 12:21:55 -0600

    LDA to QDA name fix

--- examples/linear_models/qda.py
+++ examples/linear_models/qda.py
@@ -10,14 +10,14 @@ import sega_learn.utils.metrics as mt
 X, y = make_data(n_samples=1000, n_features=2, cov_class_1=np.array([[0.0, -1.0], [2.5, 0.7]]) * 2.0, cov_class_2=np.array([[0.0, -1.0], [2.5, 0.7]]).T * 2.0, shift=[4, 1], seed=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-lda = QuadraticDiscriminantAnalysis()
-lda.fit(X_train, y_train)
-y_pred = lda.predict(X_test)
+qda = QuadraticDiscriminantAnalysis()
+qda.fit(X_train, y_train)
+y_pred = qda.predict(X_test)
 
 print(f"\nConfusion Matrix")
-cm = mt.Metrics.show_confusion_matrix(y, lda.predict(X))
+cm = mt.Metrics.show_confusion_matrix(y, qda.predict(X))
 
 print(f"\nClassification Report")
-cls = mt.Metrics.show_classification_report(y, lda.predict(X))
+cls = mt.Metrics.show_classification_report(y, qda.predict(X))
 
 
